2d86ce7479877533e7f962656d3e13b5bea76ed7
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-01-30 21:07:11 +0300

    зміна тексту

--- 31.py
+++ 31.py
@@ -1,27 +1,32 @@
 # якщо згадаєте, можете, будь ласка, на наступному уроці показати як робити масову заміну, наприклад по всьому коду замінити щось на щось
 # і чи є якісь гарячі клавіші, якими зручно користуватись
-num_1 = int(input('введіть перше число: '))
-num_2 = int(input('введіть друге число: '))
+num_1 = input('введіть перше число: ')
+num_2 = input('введіть друге число: ')
 operation = input('введіть дію: ')
 
-if num_2 >= 0: # перевірка на знак 2-го числа, щоб красиво прописати дію: з дужками або без
-    if operation == '+':
-        print(num_1,'+',num_2,'=',num_1+num_2)
-    elif operation == '-':
-        print(num_1,'-',num_2,'=',num_1-num_2)
-    elif operation == "*":
-        print(num_1,'*',num_2,'=',num_1*num_2)
-    elif operation == '/':
-        if num_2 == 0:
-            print('не можна ділити на 0!!!')
-        else:
-            print(num_1,'/',num_2,'=',num_1 / num_2)
+if  num_1.isnumeric() and num_2.isnumeric():
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+    if num_2 >= 0: # перевірка на знак 2-го числа, щоб красиво прописати дію: з дужками або без
+        if operation == '+':
+            print(num_1,'+',num_2,'=',num_1+num_2)
+        elif operation == '-':
+            print(num_1,'-',num_2,'=',num_1-num_2)
+        elif operation == "*":
+            print(num_1,'*',num_2,'=',num_1*num_2)
+        elif operation == '/':
+            if num_2 == 0:
+                print('не можна ділити на 0!!!')
+            else:
+                print(num_1,'/',num_2,'=',num_1 / num_2)
+    else:
+        if operation == '+':
+            print(num_1,'+ (',num_2,') =',num_1+num_2)
+        elif operation == '-':
+                print(num_1,'- (',num_2,') =',num_1-num_2)
+        elif operation == '*':
+            print(num_1,'* (',num_2,') =',num_1*num_2)
+        elif operation == '/': # перевірка 2-го числа на 0 не потрібна, бо у нас вище точно відомо, що друге число від'ємне
+            print(num_1,'/',num_2,') =',num_1/num_2)
 else:
-    if operation == '+':
-        print(num_1,'+ (',num_2,') =',num_1+num_2)
-    elif operation == '-':
-            print(num_1,'- (',num_2,') =',num_1-num_2)
-    elif operation == '*':
-        print(num_1,'* (',num_2,') =',num_1*num_2)
-    elif operation == '/': # перевірка 2-го числа на 0 не потрібна, бо у нас вище точно відомо, що друге число від'ємне
-        print(num_1,'/',num_2,') =',num_1/num_2)
+    print('введіть САМЕ!! числа')
