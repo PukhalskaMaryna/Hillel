ec427682ee00e1318b32ffcc3ba3a29bf69d2552
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-01-31 13:46:09 +0300

    зміна тексту

--- 31_3.py
+++ 31_3.py
@@ -1,10 +1,8 @@
 try:
-    a = float(input('Введіть перше число: ').strip())
-    b = float(input('Введіть друге число: ').strip())
+    a = float(input('Введіть перше число: '))
+    b = float(input('Введіть друге число: '))
     operation = input('Введіть дію: ').strip()
     results = {"+": a + b, "-": a - b, "*": a * b, "/": (a / b) if b != 0 else None}
     print(f'{a} {operation} {b if b >= 0 else f"({b})"} = {results[operation]}')
-except ValueError:
-    print("Введене значення не є числом")
-except ZeroDivisionError:
-    print("Не можна ділити на нуль")
+except Exception as e:
+    print(e)
