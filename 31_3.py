286f96f464578a223d5d14e00fa0db64f5d10830
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-01-31 14:23:42 +0300

    зміна тексту

--- 31_3.py
+++ 31_3.py
@@ -1,8 +1,9 @@
 try:
-    a = float(input('Введіть перше число: '))
-    b = float(input('Введіть друге число: '))
+    a,b = map(float, input("Введіть два числа через пробіл: ").split())
     operation = input('Введіть дію: ').strip()
-    results = {"+": a + b, "-": a - b, "*": a * b, "/": (a / b) if b != 0 else None}
+    results = {"+": a + b, "-": a - b, "*": a * b, "/": (a / b) if b != 0 else "Невизначеність"}
     print(f'{a} {operation} {b if b >= 0 else f"({b})"} = {results[operation]}')
-except Exception as e:
-    print(e)
+except ValueError:
+    print("Помилка: не введено коректно 2 числа")
+except KeyError:
+    print("Помилка: неправильна операція")
