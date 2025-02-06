2e2afed92b7ca10e8464110034ca202b65cc8d70
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-06 22:03:39 +0300

    зміна тексту

--- 32.py
+++ 32.py
@@ -1,7 +1,5 @@
 lst = [10,12,'3',4,[1,2,3,4]]
-if len(lst) == 0:
-    print([])
-else:
-    lst.insert(0,lst[-1])
-    print(lst[:-1])
+if lst: # чи порожній?
+    lst.insert(0,lst.pop())
+print(lst)
 
