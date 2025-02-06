e611e8c750cda6784315167bf42d6fd98bdc1ae5
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-06 22:05:17 +0300

    зміна тексту

--- 33.py
+++ 33.py
@@ -1,3 +1,3 @@
-lst = [1,888,'h',44,33]
+lst = [1, 888, 'h', 44, 33]
 # якщо к-ть елементів парна, то ділить навпіл, бо len(list_1)%2 = 0, інакше len(list_1)%2 = 1 і ділить з першою більшою к-тю елементів
-print([] if len(lst) == 0 else [lst[:len(lst) // 2 + len(lst) % 2], lst[len(lst) // 2 + len(lst) % 2:]])
+print(lst if not lst else [lst[:len(lst) // 2 + len(lst) % 2], lst[len(lst) // 2 + len(lst) % 2:]])
