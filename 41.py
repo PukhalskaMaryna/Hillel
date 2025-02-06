bd48d37fa69f70ce66f8319bab7977b0deebae10
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-06 21:40:12 +0300

    зміна тексту

--- 41.py
+++ 41.py
@@ -1,6 +1,6 @@
-lst = [0, 0, 0, 1, 0, 't', [1, 2, 0], 12, 3]
-lst0 = []
+lst = [0, 0, 0, 0, 1, 0, 't', [1, 2, 0], 12, 3]
+i = 0 # рахує к-ть 0
 while 0 in lst:
     lst.remove(0)
-    lst0 += [0]
-print(lst+lst0)
+    i += 1
+print(lst + [0] * i)
