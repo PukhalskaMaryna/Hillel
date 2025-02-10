c789a37832ae943d7e3c85748b4a18ad28e73b3b
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-10 22:13:33 +0300

    зміна тексту

--- 51.py
+++ 51.py
@@ -1,11 +1,12 @@
 import string
 import keyword
 my_variable = input("введіть ім'я змінної: ")
-print(not any((i.isupper() or i in string.punctuation.replace("_", " ") for i in my_variable)) # перебір елементів рядка і перевірка на велику літеру або чи входить в список символів
-and not (my_variable in keyword.kwlist) # не входить в список зарезервованих
-and not (my_variable[0].isdigit()) # 1 символ не є числом
-and my_variable.count("_") < 2 # рахує к-ть _
-)
+print(
+    not any((i.isupper() or i in string.punctuation.replace("_", " ") for i in my_variable)) # перебір елементів рядка і перевірка на велику літеру або чи входить в список символів
+    and not (my_variable in keyword.kwlist) # не входить в список зарезервованих
+    and not (my_variable[0].isdigit()) # 1 символ не є числом
+    and my_variable.count("_") < 2 # рахує к-ть _
+    )
 
 
 
