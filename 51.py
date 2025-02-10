0ff48fe5a4e430ca29a45b3a3cf096e2cdf27c72
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-10 22:11:41 +0300

    зміна тексту

--- 51.py
+++ 51.py
@@ -1,6 +1,6 @@
 import string
 import keyword
-my_txt = input("введіть текст: ")
+my_txt = input("введіть ім'я змінної: ")
 print(not any((i.isupper() or i in string.punctuation.replace("_", " ") for i in my_txt)) # перебір елементів рядка і перевірка на велику літеру або чи входить в список символів
 and not (my_txt in keyword.kwlist) # не входить в список зарезервованих
 and not (my_txt[0].isdigit()) # 1 символ не є числом
