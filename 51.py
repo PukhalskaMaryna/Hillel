ea3ed2842a7a30b984fceee48d3f9415e680d6a8
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-11 16:22:31 +0300

    зміна тексту

--- 51.py
+++ 51.py
@@ -5,7 +5,7 @@ print(
     not any((i.isupper() or i in string.punctuation.replace("_", " ") for i in my_variable)) # перебір елементів рядка і перевірка на велику літеру або чи входить в список символів
     and not (my_variable in keyword.kwlist) # не входить в список зарезервованих
     and not (my_variable[0].isdigit()) # 1 символ не є числом
-    and my_variable.count("_") < 2 # рахує к-ть _
+    and (my_variable.count("_") != len(my_variable) if len(my_variable) > 1 else all(""))   # рахує к-ть _
     )
 
 
