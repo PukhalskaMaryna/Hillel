fbf8aeb588f49cd289113375090256c0cd75cb25
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-11 20:43:22 +0300

    зміна тексту

--- 52.py
+++ 52.py
@@ -1,11 +1,9 @@
 except_key = "y"
 while except_key.lower().strip() == "y":
     try:
-            a = float(input("Введіть перше число: ").strip())
-            b = float(input("Введіть друге число: ").strip())
-            results = ["+", a + b, "-", a - b, "*", a * b, "/", a / b if b != 0 else "Невизначеність"]
-            operation = input('Введіть дію: ').strip()
-            print("Помилка: неправильна дія") if operation not in ("+", "-", "*", "/") else print(f'{a} {operation} {b if b >= 0 else f"({b})"} = {results[results.index(operation) + 1]}')
+            a = [float(input("Введіть перше число: ").strip()),float(input("Введіть друге число: ").strip()),input('Введіть дію: ').strip()]
+            results = ["+", a[0] + a[1], "-", a[0] - a[1], "*", a[0] * a[1], "/", a[0] / a[1] if a[1] != 0 else "Невизначеність"]
+            print("Помилка: неправильна дія") if a[2] not in ("+", "-", "*", "/") else print(f'{a[0]} {a[2]} {a[1] if a[1] >= 0 else f"({a[1]})"} = {results[results.index(a[2]) + 1]}')
     except ValueError:
         print("Помилка: не коректне число")
     except_key = input("Для продовження введіть Y: ")
