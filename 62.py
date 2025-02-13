ad788652c5d6d7a1f5e5c80bac82605d84bd80d8
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-13 22:20:28 +0300

    зміна тексту

---
+++ 62.py
@@ -0,0 +1,23 @@
+seconds = int(input("Введіть число: "))
+dic = {'d':86400,'h':3600,'m':60}
+
+for i in dic:
+    val = seconds // dic[i]
+    seconds = seconds % dic[i]
+    dic.update ({i: val})
+
+dic_day = {
+    1: "день",
+    2: "дні",
+    3: "дні",
+    4: "дні",
+    0: "днів",
+    5: "днів",
+    6: "днів",
+    7: "днів",
+    8: "днів",
+    9: "днів"
+}
+print (f'{dic['d']} {dic_day[dic['d'] % 10]} {str(dic['h']).zfill(2)}:{str(dic['m']).zfill(2)}:{str(seconds).zfill(2)}')
+
+
