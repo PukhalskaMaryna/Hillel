254a3d5edee47e52fcb467b62a8c86e6ee7dbb07
PukhlaskaMaryna <pukhalskiy@ukr.net>
2025-02-14 18:31:06 +0300

    зміна тексту

--- 61.py
+++ 61.py
@@ -1,7 +1,11 @@
 import string
 
-sym_1,sym_2 = map(ord,input("Введіть 2 літери через дефіс: ").replace(' ','').split("-"))
-for c in string.ascii_letters:
-    if ord(c) in range(sym_1, sym_2+1):
-        print(c,end = '')
-
+sym_1, sym_2 = map(ord, input("Введіть 2 літери через дефіс: ").replace(' ', '').split("-"))
+if sym_1 <= sym_2:
+    for c in string.ascii_letters: # цикл для букв, що в одному регістрі
+        if ord(c) in range(sym_1, sym_2 + 1):
+            print(c, end='')
+else:
+    for c in string.ascii_letters: # цикл для букв, де перша у нижньому, а друга у верхньому
+        if ord(c) in range(sym_1, ord('z') + 1) or ord(c) in range(ord('A'), sym_2 + 1):
+            print(c, end='')
---
+++ adv_1.py
@@ -0,0 +1,5 @@
+#with open('C:/Users/puhal/Desktop/adventofcode/1.txt', 'r') as file:
+ #   content = file.read()
+   # print(type(content))
+  #  print(type(file))
+
