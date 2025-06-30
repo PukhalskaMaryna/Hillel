a9a87efc1e21d335b9765a3d5c2cd6687e110dba
root <root@kho-6057-nb.fuib.com>
2025-06-30 22:48:21 +0300

    Comment

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 # інкапсуляція, наслідування, поліморфізм та абстракція
+
+# Інкапсуляція — це об’єднання атрибутів і методів всередині класу, 
+# яке запобігає доступу зовнішніх класів до атрибутів і методів даного класу та їх зміні. 
+# Іншими словами, це “приховування даних”.
 # class Person:
 #     def __init__(self,name,age):
 #         self.name = name
@@ -9,10 +13,72 @@
 
 # p = Person("Mary",41)
 # p2 = Person("Max",40)
-# print (p.introduce())
-# print (p2.introduce())
+# # print (p.introduce())
+# # print (p2.introduce())
 
-class BankAccount:
-    def __init__(self,owner,balance):
-        self.owner = owner
-        self._balance = balance
+# class BankAccount:
+#     def __init__(self,owner,balance):
+#         self.owner = owner
+#         self.__balance = balance
+
+#     def depos(self,amount):
+#         if amount > 0:
+#             self.__balance += amount
+#             return f'Done, + {amount}, bal {self.__balance}'
+#         return f'Negative summ'
+    
+#     def withdraw(self,amount):
+#         if 0 < amount <= self.__balance:
+#             self.__balance -= amount
+#             return f'Done, - {amount}, bal {self.__balance}'
+#         return f'No many'
+    
+#     def get_balance(self):
+#         return f'Bal: {self.__balance}'
+    
+# acc = BankAccount(p.name,1000)    
+# print(acc.get_balance())
+# print(acc.depos(500))
+# print(acc.depos(500))
+# print(acc.withdraw(500))
+
+# acc.__balance += 10000
+
+# acc._BankAccount__balance += 10000
+# acc.get_balance
+#     @property
+#     def bal(self):
+#         return self.__balance
+    
+#     @bal.setter
+#     def bal(self,amount):
+#          if amount > 0:
+#             self.__balance += amount
+
+# acc = BankAccount(p.name,1000) 
+# print(acc.bal)
+# acc.bal = 1000000000
+# print(acc.bal)
+
+
+# наслідування
+
+class Animal:
+    def __init__(self, name = 'Any'):
+        self.name = name
+
+    def make_sound(self):
+        return f'some sound - {self.name}' 
+
+class Dog(Animal):
+    def __init__(self,breed,name = 'Any dog'):
+        super().__init__(name)
+        self.breed = breed
+
+    def make_sound(self):
+        return f'{self.name} says GAU! Breed {self.breed}'
+    
+d = Dog(breed = 'Mops')
+a = Animal("Bonny")
+print(d.make_sound())
+print(a.make_sound())
